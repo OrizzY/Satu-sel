dfcc96aa92c5afbe86cc1f3d4399b37ca1c290f6
OrizzY <rozifatkhur451@gmail.com>
2025-10-08 12:32:32 +0700

    Add files via upload

--- neuron1.py
+++ neuron1.py
@@ -1,1 +1,9 @@
-print("Satu neuron nyala")
+class Neuron:
+    def __init__(self):
+        self.weight = 0.5
+        self.bias = 0.1
+    def feedForward(self,input):
+        if input:
+            self.weight + self.bias > 0
+        else:
+            0
